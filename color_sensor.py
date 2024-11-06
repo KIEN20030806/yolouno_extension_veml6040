24ff5c23c3f66ba83f35651e625e5f5a318969a1
nguyenmihn <Nminh59001@gmail.com>
2024-11-06 19:30:27 +0700

    thay doi

--- color_sensor.py
+++ color_sensor.py
@@ -1,21 +1,201 @@
-def color_sensor_init():
-    color_sensor = ColorSensorVEML6040()
-    return color_sensor
+import machine, time
+from micropython import const
+from math import exp, isnan
+from utility import say
+#from setting import *
 
-def color_sensor_get_red(color_sensor):
-    return color_sensor.get_red()
+_VEML6040_I2C_ADDRESS    = const(0x10)
 
-def color_sensor_get_green(color_sensor):
-    return color_sensor.get_green()
+# REGISTER CONF (00H) SETTINGS
 
-def color_sensor_get_blue(color_sensor):
-    return color_sensor.get_blue()
+_VEML6040_IT_40MS        = const(0x00)
+_VEML6040_IT_80MS        = const(0x10)
+_VEML6040_IT_160MS       = const(0x20)
+_VEML6040_IT_320MS       = const(0x30)
+_VEML6040_IT_640MS       = const(0x40)
 
-def color_sensor_get_lux(color_sensor):
-    return color_sensor.get_lux()
+_VEML6040_IT_1280MS      = const(0x50)
 
-def color_sensor_get_cct(color_sensor, offset):
-    return color_sensor.get_cct(offset)
+_VEML6040_TRIG_DISABLE   = const(0x00)
+_VEML6040_TRIG_ENABLE    = const(0x04)
 
-def color_sensor_read_hsv(color_sensor):
-    return color_sensor.readHSV()
+_VEML6040_AF_AUTO        = const(0x00)
+_VEML6040_AF_FORCE       = const(0x02)
+
+_VEML6040_SD_ENABLE      = const(0x00)
+_VEML6040_SD_DISABLE     = const(0x01)
+
+#COMMAND CODES
+
+_COMMAND_CODE_CONF       = const(0x00)
+_COMMAND_CODE_RED        = const(0x08)
+_COMMAND_CODE_GREEN      = const(0x09)
+_COMMAND_CODE_BLUE       = const(0x0A)
+_COMMAND_CODE_WHITE      = const(0x0B)
+
+#G SENSITIVITY
+
+_VEML6040_GSENS_40MS     = 0.25168
+_VEML6040_GSENS_80MS     = 0.12584
+_VEML6040_GSENS_160MS    = 0.06292
+_VEML6040_GSENS_320MS    = 0.03146
+_VEML6040_GSENS_640MS    = 0.01573
+_VEML6040_GSENS_1280MS   = 0.007865
+
+#chuyển đổi màu RGB thành màu HSV(màu sắc, độ băo hoa, độ sáng)
+def rgb2hsv(r, g, b):
+    r = float(r/65535)
+    g = float(g/65535)
+    b = float(b/65535)
+    high = max(r, g, b)
+    low = min(r, g, b)
+    h,s,v = high,high,high
+    d = high - low
+    s = 0 if high == 0 else d/high
+    if high == low:
+        h = 0.0
+    else:
+        h = {
+            r: (g - b) / d+(6 if g < b else 0),
+            g: (b - r) / d+2,
+            b: (r - g) / d+4,
+        }[high]
+        h /= 6
+    return {'hue':h*360,'sat':s, 'val':v}
+
+class ColorSensorVEML6040:
+    #hàm khởi tạo cho lớp cảm biến
+    def __init__(self, address=_VEML6040_I2C_ADDRESS):
+        self._i2c = machine.SoftI2C(scl=12, sda=11, freq=100000)
+        self._addr = address
+        self._config = 0
+
+        if self._i2c.scan().count(address) == 0:
+            raise Exception('Color sensor VEML6040 not found')
+
+        self.config(_VEML6040_IT_160MS + _VEML6040_AF_AUTO + _VEML6040_SD_ENABLE)
+    #cấu hinh cam bien
+    def config(self, config):
+        self._i2c.writeto(self._addr, bytes([_COMMAND_CODE_CONF, config, 0]))
+        self._config = config
+    #đọc dữ liệu từ cảm biến
+    def read(self, commandCode):
+        self._i2c.writeto(self._addr, bytes([commandCode]))
+        data = self._i2c.readfrom(self._addr, 2)
+        data = data[0] + (data[1]<<8)
+        return data
+    #trả về 3 màu đỏ xanh xanh trắng
+    def get_red(self):
+        return self.read(_COMMAND_CODE_RED)
+
+    def get_green(self):
+        return self.read(_COMMAND_CODE_GREEN)
+    
+    def get_blue(self):
+        return self.read(_COMMAND_CODE_BLUE)
+    
+    def get_white(self):
+        return self.read(_COMMAND_CODE_WHITE)
+    #hàm tính toán độ sáng
+    def get_lux(self):
+        sensorValue = self.read(_COMMAND_CODE_GREEN)
+    
+        if self._config & 0x70 == _VEML6040_IT_40MS:
+            ambientLightInLux = sensorValue * _VEML6040_GSENS_40MS
+        elif self._config & 0x70 == _VEML6040_IT_80MS:
+            ambientLightInLux = sensorValue * _VEML6040_GSENS_80MS
+        elif self._config & 0x70 == _VEML6040_IT_160MS:
+                ambientLightInLux = sensorValue * _VEML6040_GSENS_160MS
+        elif self._config & 0x70 == _VEML6040_IT_320MS:
+                ambientLightInLux = sensorValue * _VEML6040_GSENS_320MS
+        elif self._config & 0x70 == _VEML6040_IT_640MS:
+                ambientLightInLux = sensorValue * _VEML6040_GSENS_640MS
+        elif self._config & 0x70 == _VEML6040_IT_1280MS:
+                ambientLightInLux = sensorValue * _VEML6040_GSENS_1280MS
+        else:
+            ambientLightInLux = -1
+        return int(ambientLightInLux)
+    #hàm tính toán nhiệt độ
+    def get_cct(self, offset):
+        red = self.get_red()
+        green = self.get_green()
+        blue = self.get_blue()
+
+        ccti = (float(red) - float(blue)) / float(green)
+        ccti = ccti + offset
+        cct = 4278.6 * pow(ccti, -1.2455)
+
+        return cct
+    #phân loại màu dựa trên giá trị hue	
+    def classify_hue(self, hues={"red":0,"yellow":60,"green":120,"cyan":180,"blue":240,"magenta":300}, min_brightness=0):
+        d = self.readHSV()
+        if d['val'] > min_brightness:
+            key, val = min(hues.items(), key=lambda x: min(360-abs(d['hue'] - x[1]),abs(d['hue'] - x[1]))) # nearest neighbour, but it wraps!
+            return key
+        else:
+            return None
+    
+    # Read colours from VEML6040
+    # Returns raw red, green and blue readings, ambient light [Lux] and colour temperature [K]
+    
+    #đọc và trả về giá trị RGB cùng với ánh sáng và nhiệt độ màu	
+    def readRGB(self):
+        red = self.get_red()
+        green = self.get_green()
+        blue = self.get_blue()
+        white = self.get_white()
+            
+        # Generate the XYZ matrix based on https://www.vishay.com/docs/84331/designingveml6040.pdf
+        colour_X = (-0.023249*red)+(0.291014*green)+(-0.364880*blue)
+        colour_Y = (-0.042799*red)+(0.272148*green)+(-0.279591*blue)
+        colour_Z = (-0.155901*red)+(0.251534*green)+(-0.076240*blue)
+        colour_total = colour_X+colour_Y+colour_Z
+        if colour_total == 0:
+            return {"red":None,"green":None,"blue":None,"white":None,"als":None,"cct":None}
+        else:
+            colour_x = colour_X / colour_total
+            colour_y = colour_Y / colour_total
+        
+        # Use McCamy formula
+        colour_n = (colour_x - 0.3320)/(0.1858 - colour_y)
+        colour_CCT = 449.0*colour_n ** 3+3525.0*colour_n ** 2+6823.3*colour_n+5520.33
+        
+        # Calculate ambient light in Lux
+        colour_ALS = green*(self._config&0x70)
+        return {"red":red,"green":green,"blue":blue,"white":white,"als":colour_ALS,"cct":colour_CCT}
+    #hàm đọc giá trị RGB và chuyển chúng sang màu HSV
+    def readHSV(self):
+        d = self.readRGB()
+        return rgb2hsv(d['red'],d['green'],d['blue'])
+
+
+color_sensor = ColorSensorVEML6040()
+
+#simple test code
+
+#hàm này luôn chạy như CT chính để liên tục in ra giá trị màu mỗi giây
+if __name__ == '__main__':
+    while True:
+        #print('white: ', color_sensor.get_white())
+        print('red: ', color_sensor.get_red(), ' green: ', color_sensor.get_green(), ' blue: ', color_sensor.get_blue())
+        #print(color_sensor.get_lux(), color_sensor.get_cct(1))
+        #print(color_sensor.readRGB())
+        ### Example 1: Print Raw RGB Data
+        #data = color_sensor.readRGB() # Read the sensor (Colour space: Red Green Blue)
+        #print(data)
+        #red = data['red'] # extract the RGB information from data
+        #grn = data['green']
+        #blu = data['blue']
+
+        #print(str(blu) + " Blue  " + str(grn) + " Green  " + str(red) + " Red") # Print the data. Printing as BGR so the Thonny plot-colours match nicely :)
+
+        ### Example 2: Classify the colour being shown - eg. a fruit sorting machine
+        #data = color_sensor.readHSV() # Read the sensor (Colour space: Hue Saturation Value)
+        #hue = data['hue'] # extract the Hue information from data
+        #print(hue)
+
+    #   label = colourSensor.classifyHue() # Read the sensor again, this time classify the colour
+    #   print(str(label) + " Hue: " + str(hue)) # Show the label and the corresponding hue
+        time.sleep(1)
+
+    
--- definition.js
+++ definition.js
@@ -1,76 +1,167 @@
-Blockly.Blocks['color_sensor_init'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Initialize VEML6040 Color Sensor");
-    this.setColour(230);
-    this.setTooltip("Initialize the VEML6040 color sensor.");
-    this.setHelpUrl("");
-  }
-};
+// const TCS34725ColorBlock = "#ae00ae";
 
-Blockly.Blocks['color_sensor_get_red'] = {
+// Blockly.Blocks["uno_tcs34725_read"] = {
+//   init: function () {
+//     this.jsonInit({
+//       colour: TCS34725ColorBlock,
+//       tooltip: "",
+//       message0: "cảm biến màu sắc đọc giá trị %1",
+//       args0: [
+//         {
+//           type: "field_dropdown",
+//           name: "RGB",
+//           options: [
+//             ["RED", "r"],
+//             ["GREEN", "g"],
+//             ["BLUE", "b"],
+//           ],
+//         }
+//       ],
+//       output: "Number",
+//       helpUrl: "",
+//     });
+//   },
+// };
+
+// Blockly.Blocks["uno_tcs34725_detect"] = {
+//   init: function () {
+//     this.jsonInit({
+//       colour: TCS34725ColorBlock,
+//       tooltip: "",
+//       message0: "cảm biến màu sắc phát hiện màu %1",
+//       args0: [
+//         {
+//           type: "field_dropdown",
+//           name: "color",
+//           options: [
+//             ["trắng", "w"],
+//             ["đen", "d"],
+//             ["đỏ", "r"],
+//             ["xanh lá (green)", "g"],
+//             ["xanh dương (blue)", "b"],
+//             ["vàng", "y"]
+//           ],
+//         }
+//       ],
+//       output: "Boolean",
+//       helpUrl: "",
+//     });
+//   },
+// };
+
+// Blockly.Blocks["uno_tcs34725_read_lux"] = {
+//   init: function () {
+//     this.jsonInit({
+//       colour: TCS34725ColorBlock,
+//       tooltip: "",
+//       message0: "cảm biến màu sắc đọc giá trị ánh sáng",
+//       output: "Number",
+//       helpUrl: "",
+//     });
+//   },
+// };
+
+// Blockly.Blocks["uno_tcs34725_read_cct"] = {
+//   init: function () {
+//     this.jsonInit({
+//       colour: TCS34725ColorBlock,
+//       tooltip: "",
+//       message0: "cảm biến màu sắc đọc giá trị nhiệt độ",
+//       output: "Number",
+//       helpUrl: "",
+//     });
+//   },
+// };
+
+// Blockly.Python["uno_tcs34725_read"] = function (block) {
+//   var RGB = block.getFieldValue("RGB");
+//   // TODO: Assemble Python into code variable.
+//   Blockly.Python.definitions_['import_tcs34725'] = 'from tcs34725 import *';
+//   Blockly.Python.definitions_['init_tcs34725'] = 'tcs34725 = ColorSensorVEML6040()';
+//   var code = "tcs34725.read_color('" + RGB + "')";
+//   return [code, Blockly.Python.ORDER_NONE];
+// };
+
+// Blockly.Python["uno_tcs34725_detect"] = function (block) {
+//   var color = block.getFieldValue("color");
+//   // TODO: Assemble Python into code variable.
+//   Blockly.Python.definitions_['import_tcs34725'] = 'from tcs34725 import *';
+//   Blockly.Python.definitions_['init_tcs34725'] = 'tcs34725 = ColorSensorVEML6040()';
+//   var code = "tcs34725.detect('" + color + "')";
+//   return [code, Blockly.Python.ORDER_NONE];
+// };
+
+// Blockly.Python["uno_tcs34725_read_lux"] = function (block) {
+//   var RGB = block.getFieldValue("RGB");
+//   // TODO: Assemble Python into code variable.
+//   Blockly.Python.definitions_['import_tcs34725'] = 'from tcs34725 import *';
+//   Blockly.Python.definitions_['init_tcs34725'] = 'tcs34725 = ColorSensorVEML6040()';
+//   var code = "tcs34725.read_color('" + RGB + "')";
+//   return [code, Blockly.Python.ORDER_NONE];
+// };
+
+// Blockly.Python["uno_tcs34725_read_cct"] = function (block) {
+//   var RGB = block.getFieldValue("RGB");
+//   // TODO: Assemble Python into code variable.
+//   Blockly.Python.definitions_['import_tcs34725'] = 'from tcs34725 import *';
+//   Blockly.Python.definitions_['init_tcs34725'] = 'tcs34725 = ColorSensorVEML6040()';
+//   var code = "tcs34725.read_color('" + RGB + "')";
+//   return [code, Blockly.Python.ORDER_NONE];
+// };
+
+
+// Tạo một khối Blockly để đọc dữ liệu từ cảm biến VEML6040
+Blockly.Blocks['uno_veml6040_read'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Get Red Value");
-    this.setColour(230);
+        .appendField("Đọc cảm biến VEML6040");
+    this.setInputsInline(true);
     this.setOutput(true, "Number");
-    this.setTooltip("Get the red value from the color sensor.");
+    this.setColour(230);  // Màu khối
+    this.setTooltip("Đọc giá trị từ cảm biến VEML6040");
     this.setHelpUrl("");
   }
 };
 
-Blockly.Blocks['color_sensor_get_green'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Get Green Value");
-    this.setColour(230);
-    this.setOutput(true, "Number");
-    this.setTooltip("Get the green value from the color sensor.");
-    this.setHelpUrl("");
-  }
+Blockly.JavaScript['uno_veml6040_read'] = function(block) {
+  var code = 'readVeml6040Data()';  // Hàm JavaScript để đọc dữ liệu cảm biến
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.Blocks['color_sensor_get_blue'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Get Blue Value");
-    this.setColour(230);
-    this.setOutput(true, "Number");
-    this.setTooltip("Get the blue value from the color sensor.");
-    this.setHelpUrl("");
-  }
-};
+// Hàm JavaScript để đọc dữ liệu từ cảm biến VEML6040
+function readVeml6040Data() {
+  // Đây là nơi bạn sẽ viết mã JavaScript để tương tác với cảm biến, ví dụ:
+  let red = getRedValue(); // Hàm giả định để lấy giá trị màu đỏ
+  let green = getGreenValue(); // Hàm giả định để lấy giá trị màu xanh lá
+  let blue = getBlueValue(); // Hàm giả định để lấy giá trị màu xanh dương
+  return { red: red, green: green, blue: blue };
+}
 
-Blockly.Blocks['color_sensor_get_lux'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Get Lux Value");
-    this.setColour(230);
-    this.setOutput(true, "Number");
-    this.setTooltip("Get the lux value from the color sensor.");
-    this.setHelpUrl("");
-  }
-};
+// Các hàm lấy giá trị của màu sắc từ cảm biến
+function getRedValue() {
+  // Gọi mã Python hoặc API tương ứng để lấy giá trị màu đỏ từ cảm biến
+  return 100;  // Trả về giá trị giả định
+}
 
-Blockly.Blocks['color_sensor_get_cct'] = {
-  init: function() {
-    this.appendValueInput("OFFSET")
-        .setCheck("Number")
-        .appendField("Get CCT with offset");
-    this.setColour(230);
-    this.setOutput(true, "Number");
-    this.setTooltip("Get the color temperature (CCT) with a specified offset.");
-    this.setHelpUrl("");
-  }
-};
+function getGreenValue() {
+  return 150;  // Trả về giá trị giả định
+}
 
-Blockly.Blocks['color_sensor_read_hsv'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("Read HSV");
-    this.setColour(230);
-    this.setOutput(true, "Object");
-    this.setTooltip("Read RGB and convert to HSV.");
-    this.setHelpUrl("");
+function getBlueValue() {
+  return 200;  // Trả về giá trị giả định
+}
+
+// Cấu hình Blockly và sử dụng toolbox của bạn
+var workspace = Blockly.inject('blocklyDiv', {
+  toolbox: document.getElementById('toolbox')
+});
+
+// Hàm để bắt đầu chạy Blockly và xử lý mã JavaScript
+function runBlocklyCode() {
+  var code = Blockly.JavaScript.workspaceToCode(workspace);
+  try {
+    eval(code); // Thực thi mã JavaScript
+  } catch (e) {
+    console.error(e);
   }
-};
+}
