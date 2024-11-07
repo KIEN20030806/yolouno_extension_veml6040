3dabad95c11ae8e38dd4a2c4eac08cce9414f25c
nguyenmihn <Nminh59001@gmail.com>
2024-11-07 15:02:44 +0700

    thay doi

--- definition.js
+++ definition.js
@@ -51,7 +51,7 @@ Blockly.Blocks['veml6040_sensor_detect_color'] = {
           "type": "field_dropdown",
           "name": "DETECT_COLOR",
           "options": [
-            ["trắng", "white"],
+            ["màu trắng", "white"],
             ["đen", "black"],
             ["đỏ", "red"],
             ["xanh lá", "green"],
