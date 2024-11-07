20636fc6ffd32cf99ecc43551c4042e7c0c8acb4
nguyenmihn <Nminh59001@gmail.com>
2024-11-07 14:42:53 +0700

    thay doi

--- definition.js
+++ definition.js
@@ -45,7 +45,7 @@ Blockly.Blocks['veml6040_sensor_detect_color'] = {
   init: function() {
     this.jsonInit({
       "type": "veml6040_sensor_detect_color",
-      "message0": "cảm biến VEML6040 phát hiện màu %1",
+      "message0": "cảm biến VEML6040 phát hiện %1",
       "args0": [
         {
           "type": "field_dropdown",
