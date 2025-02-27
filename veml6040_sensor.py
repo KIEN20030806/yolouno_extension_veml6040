278883b211be037f5d7e9454872782c43589348e
Nguyễn Văn Kiên <138760549+KIEN20030806@users.noreply.github.com>
2025-02-27 16:17:34 +0700

    Change reading command

--- veml6040_sensor.py
+++ veml6040_sensor.py
@@ -129,7 +129,7 @@ class VEML6040Sensor:
 
         return cct
 
-    def classify_hue(self):
+    def Classify_Hue(self):
         hues = {"red": 0, "yellow": 60, "green": 120, "cyan": 180, "blue": 240, "magenta": 300}
         min_brightness = 0
         # Đọc giá trị HSV
